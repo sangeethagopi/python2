f53d3176b06752d32136cf574ac8464f747cbebe
sangeethagopi <50619394+sangeethagopi@users.noreply.github.com>
2019-07-19 09:19:35 +0530

    Update cea.py

--- cea.py
+++ cea.py
@@ -1,11 +1,11 @@
-a=list(input())
-b=list(a)
+ac=list(input())
+bc=list(ac)
 count=0
-b.reverse()
-for i in range(len(a)):
-    if(a[i]==b[i]):
+bc.reverse()
+for i in range(len(ac)):
+    if(ac[i]==bc[i]):
         count=count+1
-if(count==(len(a))):
+if(count==(len(ac))):
     print("yes")
 else:
     print("no")
